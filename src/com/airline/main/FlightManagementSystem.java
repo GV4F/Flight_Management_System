2d8d5814fd0e865716f1ae28d7ef7c3ff154b0b0
Ángel Caal <angelcaal0619@gmail.com>
2026-06-04 19:41:32 -0600

    New airline created

--- src/com/airline/main/FlightManagementSystem.java
+++ src/com/airline/main/FlightManagementSystem.java
@@ -1,6 +1,9 @@
+import com.airline.model.*;
+
 public class FlightManagementSystem {
   
   public static void main(String[] args) {
-  
+    var airline = new Airline();
+    System.out.printf("Name: %s", airline.getName());
   }
 }
